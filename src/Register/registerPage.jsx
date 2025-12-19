9a861483316800bfc4db533410fa7a0f7ba29e38
chiranjeevi siva deepak <deepuainmapudi405@gmail.com>
2025-12-19 15:29:30 +0530

    third

--- src/Register/registerPage.jsx
+++ src/Register/registerPage.jsx
@@ -20,7 +20,7 @@ function RegisterPage() {
 
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://aibackend-la7w.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
